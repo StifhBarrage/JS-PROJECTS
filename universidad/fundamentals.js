8e4dec24d1b3e6617505a0744f28be064222a869
StifhBL <stifhlife@gmail.com>
2021-09-20 16:10:38 -0500

    second
    Signed-off-by: StifhBL <stifhlife@gmail.com>

--- universidad/fundamentals.js
+++ universidad/fundamentals.js
@@ -1,5 +1,21 @@
 // IIFE function
 (function () {
-   console.log(`hello world`) 
+   console.log(`hello world`); 
 })();
 
+// ternary operator
+const login = true;
+console.log(login === true ? 'its login' : 'it isnt login');
+
+/* in javascript you have for in, for of , for, while, do while */
+
+const cars = {
+   model: 'Camaro',
+   engine: 6.1,
+   year: 1995,
+   brand: 'Chevrolet'
+}
+
+for (const car in cars) {
+   console.log(`${car} : ${cars[car]}`);
+}
